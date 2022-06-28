8e6fc0ebcfa7f23ed36fc16cd1947e5771ca736d
mumaralfajar <kuhaaku@gmail.com>
2022-06-29 06:58:55 +0700

    add search screen

--- lib/main_screens/home.dart
+++ lib/main_screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../minor_screens/search.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -16,8 +18,54 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
-          backgroundColor: Colors.white,
-          title: const CupertinoSearchTextField(),
+          backgroundColor: Colors.black,
+          title: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SearchScreen()));
+            },
+            child: Container(
+              height: 36,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1.4),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 4),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        'What are you looking for?',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 36,
+                    width: 74,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[600],
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Center(
+                      child: Text(
+                        'Search',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           bottom: const TabBar(
             isScrollable: true,
             indicatorWeight: 4,
@@ -61,7 +109,7 @@ class RepeatedTabs extends StatelessWidget {
     return Tab(
       child: Text(
         label,
-        style: TextStyle(color: Colors.grey.shade600),
+        style: const TextStyle(color: Colors.white),
       ),
     );
   }
